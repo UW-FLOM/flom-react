31cf28782be45bf3491b4d00cdcd5ea766e0f0d6
Marshall Agnew <marshall@okera.com>
2018-07-07 16:39:38 -0700

    Click to add markers

--- src/pages/MapToolPage.js
+++ src/pages/MapToolPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { map } from 'lodash';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Header, PlainText } from '../components/Typography';
-import { width } from 'window-size';
+import { PlainText } from '../components/Typography';
 
 const TILE_URL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png';
 const TILE_ATTRIBUTION = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
@@ -11,9 +11,10 @@ class MapToolPage extends Component {
     lat: 47.6202,
     lng: -122.3472,
     zoom: 13,
+    markers: []
   }
 
-  componentDidMount() {    
+  componentDidMount() {
     const leafletMap = this.leafletMap.leafletElement;
     leafletMap.on('zoomend', () => {
         window.console.log('Current zoom level -> ', leafletMap.getZoom());
@@ -23,10 +24,14 @@ class MapToolPage extends Component {
     });
     leafletMap.on('click', (e) => {
       console.log('Click event -> ', e);
+      this.setState({
+        markers: [...this.state.markers, {lat: e.latlng.lat, lng: e.latlng.lng}]
+      })
     })
   }
 
   render() {
+
     const position = [this.state.lat, this.state.lng]
     return (
       <Map 
@@ -44,13 +49,19 @@ class MapToolPage extends Component {
           attribution={TILE_ATTRIBUTION}
           url={TILE_URL}
         />
-        <Marker position={position}>
-          <Popup>
-            <PlainText>
-            A pretty CSS3 popup. Easily customizable.
-            </PlainText>
-          </Popup>
-        </Marker>
+        {
+          map(this.state.markers, (marker, idx) => {
+            return (
+              <Marker key={idx} position={[marker.lat, marker.lng]}>
+                <Popup>
+                  <PlainText>
+                    {marker.lat}, {marker.lng}
+                  </PlainText>
+                </Popup>
+              </Marker>
+            )
+          })
+        }
       </Map>
     )
   }
