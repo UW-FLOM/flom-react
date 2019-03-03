fb3478edf8ea831c20ca84ea1be8cbd65494e4f2
Marshall Agnew <marshall@okera.com>
2019-03-02 16:36:16 -0800

    Move db-setup to docker start. Change postgres user

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 /usr/lib/postgresql/10/bin/pg_ctl start
-echo '--------------->'
-pwd
-echo '--------------+'
-ls
-echo '--------------='
-ls ..
-echo '--------------*'
+yarn db:setup
 yarn start:prod
