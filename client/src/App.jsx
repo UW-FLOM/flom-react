49e9d72fe21f948264563968c1232d9c9a0febf7
Junyin Chen <vulpesvulpes825@gmail.com>
2021-10-24 17:25:51 -0700

    Code Split App.jsx (#52)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,29 @@
-import React, { useState, useEffect, Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import SurveyListPage from './pages/SurveyListPage';
-import Survey from './pages/Survey';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Survey = lazy(() => import('./pages/Survey')
+  .then(({ default: Survey }) => ({ default: Survey })));
+
+const SurveyListPage = lazy(() => import('./pages/SurveyListPage')
+  .then(({ default: SurveyListPage }) => ({ default: SurveyListPage })));
 
 // This component handles routing. Rendering the page specified
 // for each URL listed in the Switch below.
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={SurveyListPage} />
-        <Route exact path="/survey/:surveyId" component={Survey} />
-      </Switch>
+      <Router>
+        <Suspense fallback={(<Loading />)}
+        >
+          <Switch>
+            <Route exact path="/" component={SurveyListPage} />
+            <Route exact path="/survey/:surveyId" component={Survey} />
+          </Switch>
+        </Suspense>
+      </Router>
     </div>
   );
 }
