1f2205dd6b5712556d6edfded7a5ca853307e8fd
Junyin Chen <VulpesVulpes825@gmail.com>
2021-10-24 17:09:55 -0700

    Switch to React Leaflet v3 and React Freedraw

--- client/src/components/MapTool.jsx
+++ client/src/components/MapTool.jsx
@@ -1,48 +1,28 @@
-import React, { Component } from 'react';
+import React, {
+  useState, useRef, useEffect, useCallback, useMemo, forwardRef,
+} from 'react';
 
-import { get, map } from 'lodash';
+import { map } from 'lodash';
 import {
-  Map, Marker, Polygon, Popup, TileLayer,
+  MapContainer, TileLayer, Marker, Popup,
 } from 'react-leaflet';
-import FreeDraw, { CREATE, NONE } from 'leaflet-freedraw';
-import { Typography } from 'antd';
+import Freedraw, { CREATE, NONE } from 'react-leaflet-freedraw';
 import PropTypes from 'prop-types';
-
-const { Text } = Typography;
-
-const freeDraw = new FreeDraw();
 
 export const polygonFromLatLngs = (latLngs) => ({
   type: 'polygon',
   geometry: map(latLngs, (shape) => map(shape, (point) => [point.lat, point.lng])),
 });
 
-class MapTool extends Component {
-  constructor(prop) {
-    super(prop);
-    this.state = {
-      markers: [],
-      objects: this.props.objects,
-      mode: 'NONE',
-      height: '100vh',
-    };
+function currentMode(mode) {
+  if (mode === 'CREATE') {
+    return CREATE;
   }
+  return NONE;
+}
 
-  componentDidMount() {
-    this.map = this.leafletMap.leafletElement;
-    this.map.addLayer(freeDraw);
-
-    this.setState({
-      mode: this.props.mode,
-      height: '100%',
-    });
-    this.changeMode(this.props.mode);
-
-    freeDraw.on('markers', (event) => {
-      this.handleFeatureDrawn(event);
-    });
-  }
-
+function MapTool({ tileURL, tileAttribution, mode, onFeatureDrawn }) {
+  /*
   componentDidUpdate(prevProps) {
     const { mode, objects } = this.props;
     if (prevProps.objects !== objects) {
@@ -54,10 +34,6 @@ class MapTool extends Component {
     }
   }
 
-  componentWillUnmount() {
-    this.map.removeLayer(freeDraw);
-  }
-
   handleFeatureDrawn(event) {
     if (event.eventType !== 'clear') {
       if (event.latLngs !== undefined || event.latLngs.length !== 0) {
@@ -67,71 +43,53 @@ class MapTool extends Component {
       }
     }
   }
+  */
 
-  changeMode(mode) {
-    if (mode === 'NONE') {
-      freeDraw.mode(NONE);
-    }
-    if (mode === 'CREATE') {
-      freeDraw.mode(CREATE);
-    }
-  }
+  const freedrawRef = useRef(null);
 
-  render() {
-    const {
-      mode, height, objects,
-    } = this.state;
-    const {
-      center, bounds, zoom, minZoom, maxZoom
-    } = this.props;
-    const { tileURL, markers, tileAttribution } = this.props;
-    return (
-      <Map
-        style={{
-          height,
-          touchAction: 'none',
-        }}
-        center={center}
-        zoom={zoom}
-        minZoom={minZoom}
-        maxZoom={maxZoom}
-        bounds={bounds}
-        maxBounds={bounds}
-        draggable={mode === 'NONE'}
-        ref={(m) => { this.leafletMap = m; }}
-      >
-        <TileLayer
-          attribution={tileAttribution}
-          url={tileURL}
-        />
-        {
-          map(objects, (object, idx) => (
-            <Polygon
-              key={idx}
-              color="#b1ef8d"
-              positions={object.GISObject.data}
-            />
-          ))
+  const handleMarkersDraw = useCallback(
+    (event) => {
+      if (event.eventType !== 'clear') {
+        if (event.latLngs !== undefined || event.latLngs.length !== 0) {
+          onFeatureDrawn(polygonFromLatLngs(event.latLngs));
+          freedrawRef.current.clear();
         }
+      }
+    },
+    [],
+  );
 
-        {
-          map(markers, (marker, idx) => (
-            <Marker key={idx} position={[marker.lat, marker.lng]}>
-              <Popup>
-                <Typography>
-                  <Text>
-                    {marker.lat}
-                    ,
-                    {marker.lng}
-                  </Text>
-                </Typography>
-              </Popup>
-            </Marker>
-          ))
-        }
-      </Map>
-    );
-  }
+
+  const handlers = useMemo(
+    () => ({
+      markers: handleMarkersDraw,
+    }),
+    [handleMarkersDraw],
+  );
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{
+        height: '100%',
+        touchAction: 'none',
+      }}
+      doubleClickZoom={false}
+      touchZoom={false}
+    >
+      <TileLayer
+        attribution={tileAttribution}
+        url={tileURL}
+      />
+      <Freedraw
+        mode={currentMode(mode)}
+        eventHandlers={handlers}
+        ref={freedrawRef}
+      />
+    </MapContainer>
+  );
 }
 
 MapTool.propTypes = {
