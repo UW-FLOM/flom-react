ba848d73aa0433b78aa5078479e081e6b62a4b84
Junyin Chen <vulpesvulpes825@gmail.com>
2021-11-05 15:24:04 -0700

    Adding more fields to survey table (#68)

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -1,5 +1,5 @@
 const Router = require('express-promise-router');
-const db = require('../db');
+const db = require('../config/db');
 
 const router = new Router();
 // export our router to be mounted by the parent application
@@ -7,13 +7,13 @@ module.exports = router;
 
 // Getting all survey
 router.get('/', async (req, res) => {
-  const { rows } = await db.query('SELECT SurveyID, Name FROM surveys');
+  const { rows } = await db.query('SELECT survey_id, name FROM survey');
   res.status(200).json(rows);
 });
 
 // Getting specific survey based on surveyID
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const { rows } = await db.query('SELECT Detail FROM surveys WHERE SurveyID = $1', [id]);
+  const { rows } = await db.query('SELECT detail FROM survey WHERE survey_id = $1', [id]);
   res.status(200).json(rows);
 });
