c5d6d435149a77d6f3cab1ba85b467f8c4481554
Junyin Chen <vulpesvulpes825@gmail.com>
2021-11-12 12:30:55 -0800

    Add User Authentication logic to api server (#70)
    * Use jsonwebtoken to issue JWT
    * Add init.sh to initialize JWT cert
    * Use passport-jwt to verify JWT
    * Enable user login api request

---
+++ server/config/passport.js
@@ -0,0 +1,32 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db');
+
+const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
+const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+// At a minimum, you must pass the `jwtFromRequest` and `secretOrKey` properties
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256'],
+};
+
+// app.js will pass the global passport object here, and this function will configure it
+module.exports = (passport) => {
+  // The JWT payload is passed into the verify callback
+  passport.use(new JwtStrategy(options, (async (jwtPayload, done) => {
+    // We will assign the `sub` property on the JWT to the database ID of user
+    let user = await db.query('SELECT * FROM users WHERE user_id = $1', [jwtPayload.sub]);
+    user = user.rows;
+    if (user.length === 0) {
+      return done(null, false);
+    }
+    if (user) {
+      return done(null, user[0]);
+    }
+    return done(null, false);
+  })));
+};
