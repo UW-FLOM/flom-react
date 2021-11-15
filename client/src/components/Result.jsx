ec81da5a41af14646dcb68c8ddcee2a0fb107f69
uhtna <uhtna8@gmail.com>
2021-11-14 19:41:36 -0800

    aa

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -4,73 +4,87 @@ import { BsFillCheckCircleFill, BsFillExclamationCircleFill, BsFillExclamationTr
 function Result({
   itemstatus, itemtitle, itemintro
 }) {
-  if ({ itemstatus } === "success") {
-    return (
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      >
-      <BsFillCheckCircleFill
-          style={{
-            fontSize: '72px',
-            color: '#52c41a',
-          }}
-        />
-      <h1>{ itemtitle }</h1>
-      <h1>{ itemintro }</h1>
-    );
-  } else if ({ itemstatus } === "error") {
-    return (
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      >
-      <BsFillExclamationTriangleFill
-          style={{
-            fontSize: '72px',
-            color: '#52c41a',
-          }}
-        />
-      <h1>{ itemtitle }</h1>
-      <h1>{ itemintro }</h1>
-    );
-  }
-
-      if ({ itemstatus } === "success") {
-        
-      
-        <BsFillExclamationTriangleFill
-          style={{
-            fontSize: '72px',
-            color: '#52c41a',
-          }}
-        />
-      } else if ({ itemstatus } === "info") {
-        <BsFillExclamationCircleFill
-          style={{
-            fontSize: '72px',
-            color: '#52c41a',
-          }}
-        />
-      } else if ({ itemstatus } === "warning") {
-        <BsFillExclamationCircleFill
-          style={{
-            fontSize: '72px',
-            color: '#52c41a',
-          }}
-        />
-      } 
-      <h1>{ itemtitle }</h1>
-      <h1>{ itemintro }</h1>
+  return (
+    <h1>test</h1>
+    /*
+    <div>
+      {(() => {
+        if ( { itemstatus } === "success" ) {
+          return (
+            <div style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            >
+              <BsFillCheckCircleFill
+                style={{
+                  fontSize: '72px',
+                  color: '#52c41a',
+                }}
+              />
+            </div>
+          )
+        } else if ( { itemstatus } === "error" ) {
+          return (
+            <div style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            >
+              <BsFillExclamationTriangleFill
+                style={{
+                  fontSize: '72px',
+                  color: '#52c41a',
+                }}
+              />
+            </div>
+          )
+        } else if ( { itemstatus } === "info" ) {
+          return (
+            <div style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            >
+              <BsFillExclamationCircleFill
+                style={{
+                  fontSize: '72px',
+                  color: '#52c41a',
+                }}
+              />
+            </div>
+          )
+        } else if ( { itemstatus } === "warning" ) {
+          return (
+            <div style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            >
+              <BsFillExclamationCircleFill
+                style={{
+                  fontSize: '72px',
+                  color: '#52c41a',
+                }}
+              />
+            </div>
+          )
+        } 
+      })()}
     </div>
+  */
   );
 }
 
