8f7240cc14efe9af03e405f04fe3245334a44468
uhtna <uhtna8@gmail.com>
2021-11-14 19:25:50 -0800

    test

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,17 +1,75 @@
 import React from 'react';
-import { BsFillCheckCircleFill } from 'react-icons/bs'; //get logos from here
-import { Result, Button } from 'antd';
+import { BsFillCheckCircleFill, BsFillExclamationCircleFill, BsFillExclamationTriangleFill } from 'react-icons/bs'; //get logos from here
 
 function Result({
   itemstatus, itemtitle, itemintro
 }) {
-  return (
-    <div>
-      <Result
-        status= {itemstatus}
-        title= {itemtitle}
-        subTitle= {itemintro}
-      />
+  if ({ itemstatus } === "success") {
+    return (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+      <BsFillCheckCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      <h1>{ itemtitle }</h1>
+      <h1>{ itemintro }</h1>
+    );
+  } else if ({ itemstatus } === "error") {
+    return (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+      <BsFillExclamationTriangleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      <h1>{ itemtitle }</h1>
+      <h1>{ itemintro }</h1>
+    );
+  }
+
+      if ({ itemstatus } === "success") {
+        
+      
+        <BsFillExclamationTriangleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      } else if ({ itemstatus } === "info") {
+        <BsFillExclamationCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      } else if ({ itemstatus } === "warning") {
+        <BsFillExclamationCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      } 
+      <h1>{ itemtitle }</h1>
+      <h1>{ itemintro }</h1>
     </div>
   );
 }
