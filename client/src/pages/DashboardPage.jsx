10a258062fb6d3d9eae3616a94dd8abb2b5e5593
Junyin Chen <vulpesvulpes825@gmail.com>
2021-11-16 16:19:41 -0800

    Add Basic Dashboard with secure API call (#79)

---
+++ client/src/pages/DashboardPage.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import user from '../services/user';
+
+function DashboardPage() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    user.test().then(
+      (response) => {
+        setContent(JSON.stringify(response.data));
+      },
+      (error) => {
+        const _content = (error.response && error.response.data)
+          || error.message
+          || error.toString();
+
+        setContent(_content);
+      },
+    );
+  }, []);
+
+  return (
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>FLOM</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/dashboard/survey">Survey</Nav.Link>
+            <Nav.Link href="#survey">Response</Nav.Link>
+          </Nav>
+          <Nav className="justify-content-end">
+            <Nav.Link href="/logout">Log Out</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default DashboardPage;
