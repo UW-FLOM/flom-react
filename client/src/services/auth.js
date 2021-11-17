e486ce34adb4508e01151bb77bd741834afa3a12
Junyin Chen <VulpesVulpes825@gmail.com>
2021-11-16 16:12:18 -0800

    Add login logic with api call using axios

---
+++ client/src/services/auth.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_URL = '/api/auth/';
+
+function logout() {
+  // remove user from local storage to log user out
+  localStorage.removeItem('currentUser');
+}
+
+function login(username, password) {
+  return axios
+    .post(`${API_URL}login/local`, {
+      username,
+      password,
+    })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem('currentUser', JSON.stringify(response.data.token));
+      }
+      return response.data;
+    });
+}
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem('currentUser'));
+};
+
+export const auth = {
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default auth;
