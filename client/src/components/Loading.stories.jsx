0f3c071f1c7389cf5ed3d76f64adad560eb8c92b
Junyin Chen <VulpesVulpes825@gmail.com>
2021-11-19 18:21:27 -0800

    Add init storybook file

---
+++ client/src/components/Loading.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Loading from './Loading';
+
+export default {
+	component: Loading,
+	title: 'Components/Loading',
+}
+
+export const Default = () => <Loading />;
