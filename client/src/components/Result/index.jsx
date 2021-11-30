a36c1d9df0748a0dbb5760e808335b3043cd08f8
uhtna <uhtna8@gmail.com>
2021-11-29 23:33:05 -0800

    Update index.jsx

--- client/src/components/Result/index.jsx
+++ client/src/components/Result/index.jsx
@@ -1,24 +1,87 @@
-import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { BsFillCheckCircleFill, BsFillExclamationCircleFill, BsFillExclamationTriangleFill } from 'react-icons/bs'; //get logos from here
 
-const Result = function Result() {
-  return (
-    <div
-      style={{
+function Result(status, title, intro) {
+  let comp;
+
+  if ( { status } === "success" ) {
+    comp = (
+      <div style={{
         width: '100%',
         height: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
       }}
-    >
-      <BsFillCheckCircleFill
-        style={{
-          fontSize: '72px',
-          color: '#52c41a',
-        }}
-      />
+      >
+        <BsFillCheckCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      </div>
+    )
+  } else if ( { status } === "error" ) {
+    comp = ( 
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+        <BsFillExclamationTriangleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      </div>
+    )
+  } else if ( { status } === "info" ) {
+    comp = (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+        <BsFillExclamationCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      </div>
+    )
+  } else if ( { status } === "warning" ) {
+    comp = (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+        <BsFillExclamationCircleFill
+          style={{
+            fontSize: '72px',
+            color: '#52c41a',
+          }}
+        />
+      </div>
+    )
+  }
+  
+  return (
+    <div>
+      { status, title, intro }
     </div>
   );
-};
+}
 
 export default Result;
