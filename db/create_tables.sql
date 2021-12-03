b9a02727d27f6ba53a29bb07da2f2d6d8992f10d
Junyin Chen <vulpesvulpes825@gmail.com>
2021-12-02 19:43:57 -0800

    feat: Add more Survey related table to db (#118)
    feat: Add page_type, page_list, page_group table
    fix: Rename all table's id to id
    fix: Change query in client and api to reflect the change to id

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,38 +1,42 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS survey (
-    survey_id VARCHAR NOT NULL,
+    id VARCHAR NOT NULL,
     name TEXT NOT NULL,
     description TEXT,
     author TEXT,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    visibility_id INT,
+    page_group_id INT,
     detail json,
-    PRIMARY KEY(survey_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS subject (
-    subject_id UUID NOT NULL,
+    id UUID NOT NULL,
     survey_id VARCHAR NOT NULL,
-    PRIMARY KEY(subject_id),
+    PRIMARY KEY(id),
     CONSTRAINT fk_survey_id
         FOREIGN KEY(survey_id)
-            REFERENCES survey(survey_id)
+            REFERENCES survey(id)
 );
 
 CREATE TABLE IF NOT EXISTS response (
     subject_id UUID NOT NULL,
-	start_time TIME,
-	end_time TIME,
+	start_time TIMESTAMP,
+	end_time TIMESTAMP,
   	detail json,
     PRIMARY KEY(subject_id),
     CONSTRAINT fk_subject_iD
         FOREIGN KEY(subject_id)
-            REFERENCES subject(subject_id)
+            REFERENCES subject(id)
 );
 
 CREATE TABLE IF NOT EXISTS user_right (
-    user_right_id SMALLSERIAL,
+    id SMALLSERIAL,
     detail VARCHAR,
-    PRIMARY KEY(user_right_id)
+    PRIMARY KEY(id)
 );
 
 INSERT INTO
@@ -42,9 +46,9 @@ VALUES
     ('User');
 
 CREATE TABLE IF NOT EXISTS user_type (
-    user_type_id SMALLSERIAL,
+    id SMALLSERIAL,
     detail VARCHAR,
-    PRIMARY KEY(user_type_id)
+    PRIMARY KEY(id)
 );
 
 INSERT INTO
@@ -54,39 +58,78 @@ VALUES
     ('SAML');
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id UUID NOT NULL,
+    id UUID NOT NULL,
     user_right_id INT NOT NULL,
     user_type_id INT NOT NULL,
     name VARCHAR NOT NULL,
-    PRIMARY KEY(user_id),
+    PRIMARY KEY(id),
     CONSTRAINT fk_user_right_id
         FOREIGN KEY(user_right_id)
-            REFERENCES user_right(user_right_id),
+            REFERENCES user_right(id),
     CONSTRAINT fk_user_type_id
         FOREIGN KEY(user_type_id)
-            REFERENCES user_type(user_type_id)
+            REFERENCES user_type(id)
 );
 
-CREATE TABLE local_user(
+CREATE TABLE IF NOT EXISTS local_user(
     user_id UUID NOT NULL,
     username varchar NOT NULL,
     password varchar NOT NULL,
     PRIMARY KEY(user_id, username),
     CONSTRAINT fk_user_id
         FOREIGN KEY(user_id)
-            REFERENCES users(user_id)
+            REFERENCES users(id)
 );
 
+-- Initialize the admin account with password "mypassword"
 WITH admin_id AS (
     INSERT INTO
-        users (user_id, user_right_id, user_type_id, name)
+        users (id, user_right_id, user_type_id, name)
         VALUES (uuid_generate_v4(), 1, 1, 'Administrator')
-        RETURNING user_id
+        RETURNING id
 )
 INSERT INTO
     local_user(user_id, username, password)
 VALUES
     ((SELECT * FROM admin_id), 'admin', '$2a$10$gV8Z9cUxiJb8HFWlPB4px.Bk7v8kcWNk1mshRa7pa8q9m5LvsuQx6');
+
+CREATE TABLE IF NOT EXISTS page_type (
+    id SMALLSERIAL,
+    detail VARCHAR,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO
+    page_type (detail)
+VALUES
+    ('form'),
+    ('map');
+
+CREATE TABLE IF NOT EXISTS page_list(
+    id UUID NOT NULL,
+    page_type_id INT NOT NULL,
+    next_page UUID,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_page_type_id
+        FOREIGN KEY(page_type_id)
+            REFERENCES page_type(id),
+    CONSTRAINT fk_next_page
+        FOREIGN KEY(next_page)
+            REFERENCES page_list(id)
+);
+
+CREATE TABLE IF NOT EXISTS page_group(
+    id UUID NOT NULL,
+    survey_id VARCHAR NOT NULL,
+    first_page UUID NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_survey_id
+        FOREIGN KEY(survey_id)
+            REFERENCES survey(id),
+    CONSTRAINT fk_first_page
+        FOREIGN KEY(first_page)
+            REFERENCES page_list(id)
+);
 
 /*
 CREATE TABLE IF NOT EXISTS Question_Types (
