947faf397a20e6364d5fb77ced602205b71bcbf2
Junyin Chen <vulpesvulpes825@gmail.com>
2021-12-06 17:28:23 -0800

    feat: Improve secret init with yarn script (#120)
    * feat: Improve secret init with yarn script
    
    feat: Add init:secret in package.json to allow easier secret init
    feat: Improve init.sh by generate secrets in secret folder
    fix: Delete id_rsa_priv_pem.pub after running init.sh
    feat: Docker-compose will load secrets via volume
    fix: Server's config will use new secrets' path
    fix: .gitignore will ignore secreat file for security
    fix: Stop display key in terminal
    fix: docker-compose production will use new secret folder

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+mkdir -p secret && cd secret
 FILE=id_rsa_pub.pem
 if [ -f "$FILE" ]; then
     echo "Already initialized, skipping."
@@ -5,7 +6,6 @@ else
     ssh-keygen -t rsa -b 4096 -m PEM -f id_rsa_priv.pem -q -N ""
     # Don't add passphrase
     openssl rsa -in id_rsa_priv.pem -pubout -outform PEM -out id_rsa_pub.pem
-    cat id_rsa_priv.pem
-    cat id_rsa_pub.pem
+    rm id_rsa_priv.pem.pub
     echo "Key initialized."
 fi
--- server/config/jwt.js
+++ server/config/jwt.js
@@ -2,7 +2,7 @@ const jsonwebtoken = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 
-const pathToKey = path.join(__dirname, '..', 'id_rsa_priv.pem');
+const pathToKey = path.join(__dirname, '..', '/secret/', 'id_rsa_priv.pem');
 const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
 
 module.exports = (user) => {
