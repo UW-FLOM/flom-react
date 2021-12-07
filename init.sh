ce0514e938982ee1f75f9ef485476614b64553fd
Junyin Chen <VulpesVulpes825@gmail.com>
2021-12-06 17:24:26 -0800

    fix: Stop display key in terminal

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ FILE=id_rsa_pub.pem
 if [ -f "$FILE" ]; then
     echo "Already initialized, skipping."
 else
-    ssh-keygen -t rsa -b 4096 -m PEM -f id_rsa_priv.pem -q -N "" > /dev/null
+    ssh-keygen -t rsa -b 4096 -m PEM -f id_rsa_priv.pem -q -N ""
     # Don't add passphrase
     openssl rsa -in id_rsa_priv.pem -pubout -outform PEM -out id_rsa_pub.pem
     rm id_rsa_priv.pem.pub
